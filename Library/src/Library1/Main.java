6482be7c65f287aa18f297c6dfd65c26fc8732dc
GreenArt <GreenArt@DESKTOP-MPS6RG1>
2020-09-18 15:09:08 +0900

    도서 관리 프로그램 - 자바 이클립스

---
+++ Library/src/Library1/Main.java
@@ -0,0 +1,15 @@
+package Library1;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+
+		Book bk = new Book();
+		
+		bk.showMain();
+		
+		
+	}
+
+}
